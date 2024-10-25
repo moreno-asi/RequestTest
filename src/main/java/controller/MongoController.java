654b36194c9ff02ffa9d253af1399b211813007e
raul.morenor <raul.morenor@i3television.es>
2024-10-25 13:06:46 +0200

    Prueba request

--- src/main/java/controller/MongoController.java
+++ src/main/java/controller/MongoController.java
@@ -1,16 +1,11 @@
 package controller;
 
-import model.AgencyNews;
-import model.Image;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import service.MongoService;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/mongo")
@@ -19,17 +14,17 @@ public class MongoController {
     @Autowired
     private MongoService mongoService;
 
-    @GetMapping("/db1")
-    public List<Image> getFromDatabase1() {
-        return mongoService.getAllFromDatabase1();
-    }
+//    @GetMapping("/db1")
+//    public List<Image> getFromDatabase1() {
+//        return mongoService.getAllFromDatabase1();
+//    }
+//
+//    @GetMapping("/db2")
+//    public List<AgencyNews> getFromDatabase2() {
+//        return mongoService.getAllFromDatabase2();
+//    }
 
-    @GetMapping("/db2")
-    public List<AgencyNews> getFromDatabase2() {
-        return mongoService.getAllFromDatabase2();
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping("/health")
     public HttpStatus health(){
         return HttpStatus.OK;
     }
